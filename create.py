8f2595cf7a59be721373005bc05a40903fd5899b
Shohjahon <hasanboyevfahriddin17@gmail.com>
2026-09-11 16:57:55 +0500

    Update create.py

--- create.py
+++ create.py
@@ -3,7 +3,6 @@ import sqlite3
 def init_db():
     conn = sqlite3.connect("fitness.db")
     cursor = conn.cursor()
-    
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS members (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,7 +12,6 @@ def init_db():
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     """)
-    
     conn.commit()
     conn.close()
 
